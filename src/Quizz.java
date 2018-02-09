19cab617f7e5766bae2e019e9c544e8f2d0379c0
Utilisateur <Utilisateur@BNPPF013.home>
2018-02-09 15:19:56 +0100

    Execution OK ?

--- src/Quizz.java
+++ src/Quizz.java
@@ -8,9 +8,10 @@ public class Quizz {
 
     System.out.println("Hello !");
     
+    String tabQuesAnsw[][] = initQuesAnsw();
     
     int score = 0;
-    for (int i=0;i<3;i++ ) {
+    for (int i=0;i<tabQuesAnsw.length;i++ ) {
       score = quesAnsw(tabQuesAnsw[i] [0],tabQuesAnsw[i] [1],score);
     }
      System.out.println("Votre score est de " + score + " points !");
@@ -20,16 +21,19 @@ public class Quizz {
   private static String [][] initQuesAnsw(){
       System.out.println("How many questions do you want play ?");
         int nbQues = input.nextInt();
-        String [][] tab = new String [nbQues][2]
+        input.nextLine();
+        String [][] tab = new String [nbQues][2];
         for (int i = 0; i < tab.length; i++) {
         	System.out.println("Get your question n° " + (i+1) + " : ");
-        	tab(i,0) = 	input.nexLine();
+        	tab[i][0] = 	input.nextLine();
         	System.out.println("Get its answer n° " + (i+1) + " : ");
-        	tab(i,1) = 	input.nexLine();
-		}      
+        	tab[i][1] = 	input.nextLine();
+		}    
+        return tab ;
   }
     private static int quesAnsw(String q, String a, int score){
-      System.out.println(q); 
+    	//input.nextLine();
+    	System.out.println(q); 
         if (input.nextLine().equalsIgnoreCase(a))
           {score++;
           System.out.println("Good answer, congratulations !");
